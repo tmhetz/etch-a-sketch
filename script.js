b9e7da360c2431ab568a2b4d520fe8ab3f7d916f
Thomas Hetzel <tmhetz@gmail.com>
2022-11-22 17:28:01 -0600

    Implement the makeGrid function
    The makeGrid function takes the grid size from the button pressed
    to generate a grid of div elements of size by size dimensions. The
    div elements are also placed in a container.

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 let btnMakeGrid = document.getElementById("makeGrid");
+let container = document.getElementById("container");
+
 btnMakeGrid.addEventListener("click", makeGrid);
 
 function makeGrid(e){
-    console.log("hit");
+    let go = true;
+    let counter = 0;
+    let number = Number(this.dataset.gridsize);
+    while(go){
+        makeRow(number);
+        counter++;
+        if(counter === number) go = false;
+    }
+}
+
+function makeRow(number){
+    let rowContainer = document.createElement("div");
+    while(number > 0){
+        let box = document.createElement("div");
+        box.classList.add("box");
+        rowContainer.appendChild(box);
+        number--;
+    }
+    container.appendChild(rowContainer);
 }
