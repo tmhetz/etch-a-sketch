67371e48c5d236b7f20e537d3d9991be08dc8029
Thomas Hetzel <tmhetz@gmail.com>
2022-11-22 18:10:04 -0600

    Implement the setWidthHeight function
    The setWidthHeight function adjusts the sizes of the sketch board
    boxes to fit the width and height of the coloring area regardless
    of the size of the grid.

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-let btnMakeGrid = document.getElementById("makeGrid");
+let btnMakeGrid64 = document.getElementById("makeGrid64");
+let btnMakeGrid32 = document.getElementById("makeGrid32");
+let btnMakeGrid16 = document.getElementById("makeGrid16");
+let btnMakeGrid8 = document.getElementById("makeGrid8");
+
 let container = document.getElementById("container");
 
-btnMakeGrid.addEventListener("click", makeGrid);
+btnMakeGrid64.addEventListener("click", makeGrid);
+btnMakeGrid32.addEventListener("click", makeGrid);
+btnMakeGrid16.addEventListener("click", makeGrid);
+btnMakeGrid8.addEventListener("click", makeGrid);
+
+// add event listeners to each button in one line
+
+// clear the sketchboard during each button press
 
 function makeGrid(e){
     let go = true;
     let counter = 0;
     let number = Number(this.dataset.gridsize);
+    
     while(go){
         makeRow(number);
         counter++;
         if(counter === number) go = false;
     }
+    setWidthHeight(number);
 }
 
 function makeRow(number){
@@ -23,4 +36,16 @@ function makeRow(number){
         number--;
     }
     container.appendChild(rowContainer);
+}
+
+function setWidthHeight(number){
+    let height = 500/number;
+    console.log(height);
+    let width = 500/number;
+    let boxes = document.getElementsByClassName("box");
+    console.log(boxes);
+    for(let i = 0; i < boxes.length; i++){
+        boxes[i].style.width = `${width}px`;
+        boxes[i].style.height = `${height}px`;
+    }
 }
